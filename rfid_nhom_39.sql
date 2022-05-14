97385b74557a5e10b686f8a5241fd60b0078d752
Kiều Đặng Kim Bảo <kieudangkimbao@gmail.com>
2022-05-14 21:53:42 +0700

    [Modified]: Cap nhat file sql

--- rfid_nhom_39.sql
+++ rfid_nhom_39.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th4 13, 2022 lúc 08:41 AM
--- Phiên bản máy phục vụ: 10.4.22-MariaDB
--- Phiên bản PHP: 8.1.1
+-- Thời gian đã tạo: Th5 14, 2022 lúc 04:51 PM
+-- Phiên bản máy phục vụ: 10.4.24-MariaDB
+-- Phiên bản PHP: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,23 +24,125 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Cấu trúc bảng cho bảng `botri`
+--
+
+CREATE TABLE `botri` (
+  `MaLH` char(20) NOT NULL,
+  `MaPH` char(20) NOT NULL,
+  `MaTH` int(11) NOT NULL,
+  `Ngay` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `botri`
+--
+
+INSERT INTO `botri` (`MaLH`, `MaPH`, `MaTH`, `Ngay`) VALUES
+('SCD1', 'C.C107', 6, '2022-05-14'),
+('SCD1', 'C.C107', 7, '2022-05-14');
+
+-- --------------------------------------------------------
+
+--
 -- Cấu trúc bảng cho bảng `diemdanh`
 --
 
 CREATE TABLE `diemdanh` (
   `MaSV` char(50) NOT NULL,
+  `MaLH` char(20) NOT NULL,
   `GioVao` datetime NOT NULL,
-  `GioRa` datetime DEFAULT NULL,
+  `GioRa` datetime NOT NULL,
   `VaoTre` tinyint(1) NOT NULL,
-  `VeSom` tinyint(1) NOT NULL
+  `VeSom` tinyint(1) NOT NULL,
+  `Vang` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `hoc`
+--
+
+CREATE TABLE `hoc` (
+  `MaSV` char(50) NOT NULL,
+  `MaLH` char(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Đang đổ dữ liệu cho bảng `diemdanh`
+-- Đang đổ dữ liệu cho bảng `hoc`
 --
 
-INSERT INTO `diemdanh` (`MaSV`, `GioVao`, `GioRa`, `VaoTre`, `VeSom`) VALUES
-('SV1', '2022-04-11 00:00:00', '2022-04-12 00:00:00', 0, 0);
+INSERT INTO `hoc` (`MaSV`, `MaLH`) VALUES
+('00B0 7A14 2C2B 2848 0800 0166', 'SCD1'),
+('300F 4F57 3AD0 01C0 8369 A241', 'SCD1'),
+('300F 4F57 3AD0 01C0 8369 A245', 'SCD1'),
+('E280 1160 6000 0209 58CD F86E', 'SCD1');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `lophoc`
+--
+
+CREATE TABLE `lophoc` (
+  `MaLH` char(20) NOT NULL,
+  `MaMH` char(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `lophoc`
+--
+
+INSERT INTO `lophoc` (`MaLH`, `MaMH`) VALUES
+('CCNLTHD1', '841072'),
+('CCNLTHD2', '841072'),
+('CCNLTHD3', '841072'),
+('CCNLTHD4', '841072'),
+('CCNLTHD5', '841072'),
+('SCD1', '841073'),
+('SCD2', '841073'),
+('SCD3', '841073'),
+('SCD4', '841073'),
+('SCD5', '841073');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `monhoc`
+--
+
+CREATE TABLE `monhoc` (
+  `MaMH` char(20) NOT NULL,
+  `TenMH` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `monhoc`
+--
+
+INSERT INTO `monhoc` (`MaMH`, `TenMH`) VALUES
+('841072', 'Các công nghệ lập trình hiện đại'),
+('841073', 'Seminar chuyên đề');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `phonghoc`
+--
+
+CREATE TABLE `phonghoc` (
+  `MaPH` char(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `phonghoc`
+--
+
+INSERT INTO `phonghoc` (`MaPH`) VALUES
+('C.A503'),
+('C.B004'),
+('C.C107');
 
 -- --------------------------------------------------------
 
@@ -54,46 +156,134 @@ CREATE TABLE `sinhvien` (
   `GT` varchar(4) NOT NULL,
   `NgSinh` date NOT NULL,
   `SDT` char(11) DEFAULT NULL,
-  `DChi` varchar(200) DEFAULT NULL,
-  `Lop` char(10) DEFAULT NULL
+  `DChi` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Đang đổ dữ liệu cho bảng `sinhvien`
 --
 
-INSERT INTO `sinhvien` (`MaSV`, `HoTen`, `GT`, `NgSinh`, `SDT`, `DChi`, `Lop`) VALUES
-('00B07A142C2B284808000166', 'Nguyen Tri Thong', 'Nu', '2022-04-07', '0342617367', '456 LMN', 'DCT1181'),
-('E28011606000020958CDF86E', 'Ly Cay Bong', 'Nam', '2022-04-05', '0342617365', '123 GMH', 'DCT1185'),
-('SV1', 'Nguyen Van A', 'Nam', '2022-04-05', '0342617369', '123 abc', 'DCT1183'),
-('SV2', 'Nguyen Thi Ban', 'Nu', '2022-04-07', '0342617368', '123 xyz', 'DCT1184');
+INSERT INTO `sinhvien` (`MaSV`, `HoTen`, `GT`, `NgSinh`, `SDT`, `DChi`) VALUES
+('00B0 7A14 2C2B 2848 0800 0166', 'Nguyen Thi Ban', 'Nu', '2022-04-07', NULL, NULL),
+('300F 4F57 3AD0 01C0 8369 A241', 'Huynh Tu Ba', 'Nam', '2022-04-06', NULL, NULL),
+('300F 4F57 3AD0 01C0 8369 A245', 'Nguyen Tri Thuc', 'Nu', '2022-05-05', NULL, NULL),
+('E280 1160 6000 0209 58CD F86E', 'Nguyen Van A', 'Nam', '2022-04-05', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tiethoc`
+--
+
+CREATE TABLE `tiethoc` (
+  `MaTH` int(11) NOT NULL,
+  `Buoi` varchar(10) NOT NULL,
+  `TGBD` time NOT NULL,
+  `TGKT` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `tiethoc`
+--
+
+INSERT INTO `tiethoc` (`MaTH`, `Buoi`, `TGBD`, `TGKT`) VALUES
+(1, 'Sang', '07:00:00', '07:50:00'),
+(2, 'Sang', '07:50:00', '08:40:00'),
+(3, 'Sang', '09:00:00', '09:50:00'),
+(4, 'Sang', '09:50:00', '10:40:00'),
+(5, 'Sang', '10:40:00', '11:30:00'),
+(6, 'Chieu', '13:00:00', '13:50:00'),
+(7, 'Chieu', '13:50:00', '14:40:00'),
+(8, 'Chieu', '15:00:00', '15:50:00'),
+(9, 'Chieu', '15:50:00', '16:40:00'),
+(10, 'Chieu', '16:40:00', '17:30:00');
 
 --
 -- Chỉ mục cho các bảng đã đổ
 --
 
 --
+-- Chỉ mục cho bảng `botri`
+--
+ALTER TABLE `botri`
+  ADD PRIMARY KEY (`MaLH`,`MaPH`,`MaTH`),
+  ADD KEY `botri_Fk_MaPH_MaPH` (`MaPH`),
+  ADD KEY `botri_MaTH_MaTH` (`MaTH`);
+
+--
 -- Chỉ mục cho bảng `diemdanh`
 --
 ALTER TABLE `diemdanh`
-  ADD PRIMARY KEY (`MaSV`,`GioVao`);
+  ADD PRIMARY KEY (`MaSV`,`MaLH`,`GioVao`);
+
+--
+-- Chỉ mục cho bảng `hoc`
+--
+ALTER TABLE `hoc`
+  ADD PRIMARY KEY (`MaSV`,`MaLH`),
+  ADD KEY `hoc_Fk_MaLH_MaLH` (`MaLH`);
+
+--
+-- Chỉ mục cho bảng `lophoc`
+--
+ALTER TABLE `lophoc`
+  ADD PRIMARY KEY (`MaLH`),
+  ADD KEY `lophoc_Fk_MaMH_MaMH` (`MaMH`);
+
+--
+-- Chỉ mục cho bảng `monhoc`
+--
+ALTER TABLE `monhoc`
+  ADD PRIMARY KEY (`MaMH`);
+
+--
+-- Chỉ mục cho bảng `phonghoc`
+--
+ALTER TABLE `phonghoc`
+  ADD PRIMARY KEY (`MaPH`);
 
 --
 -- Chỉ mục cho bảng `sinhvien`
 --
 ALTER TABLE `sinhvien`
-  ADD PRIMARY KEY (`MaSV`),
-  ADD UNIQUE KEY `SDT` (`SDT`);
+  ADD PRIMARY KEY (`MaSV`);
+
+--
+-- Chỉ mục cho bảng `tiethoc`
+--
+ALTER TABLE `tiethoc`
+  ADD PRIMARY KEY (`MaTH`);
 
 --
 -- Các ràng buộc cho các bảng đã đổ
 --
 
 --
+-- Các ràng buộc cho bảng `botri`
+--
+ALTER TABLE `botri`
+  ADD CONSTRAINT `botri_Fk_MaLH_MaLH` FOREIGN KEY (`MaLH`) REFERENCES `lophoc` (`MaLH`),
+  ADD CONSTRAINT `botri_Fk_MaPH_MaPH` FOREIGN KEY (`MaPH`) REFERENCES `phonghoc` (`MaPH`),
+  ADD CONSTRAINT `botri_MaTH_MaTH` FOREIGN KEY (`MaTH`) REFERENCES `tiethoc` (`MaTH`);
+
+--
 -- Các ràng buộc cho bảng `diemdanh`
 --
 ALTER TABLE `diemdanh`
-  ADD CONSTRAINT `diemdanh_ibfk_1` FOREIGN KEY (`MaSV`) REFERENCES `sinhvien` (`MaSV`);
+  ADD CONSTRAINT `diemdanh_Fk_MaSV_MaLH_MaSV_MaLH` FOREIGN KEY (`MaSV`,`MaLH`) REFERENCES `hoc` (`MaSV`, `MaLH`);
+
+--
+-- Các ràng buộc cho bảng `hoc`
+--
+ALTER TABLE `hoc`
+  ADD CONSTRAINT `hoc_Fk_MaLH_MaLH` FOREIGN KEY (`MaLH`) REFERENCES `lophoc` (`MaLH`),
+  ADD CONSTRAINT `hoc_Fk_MaSV_MaSV` FOREIGN KEY (`MaSV`) REFERENCES `sinhvien` (`MaSV`);
+
+--
+-- Các ràng buộc cho bảng `lophoc`
+--
+ALTER TABLE `lophoc`
+  ADD CONSTRAINT `lophoc_Fk_MaMH_MaMH` FOREIGN KEY (`MaMH`) REFERENCES `monhoc` (`MaMH`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
